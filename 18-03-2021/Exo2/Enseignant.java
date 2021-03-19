49ff2be6d4cf602b9df613985902d8c83d853709
bakedpotato191 <vladislavp@pm.me>
2021-03-19 11:39:23 +0100

    upd

--- 18-03-2021/Exo2/Enseignant.java
+++ 18-03-2021/Exo2/Enseignant.java
@@ -2,26 +2,28 @@ package exo2;
 
 import java.util.concurrent.atomic.AtomicInteger;
 
-public class Enseignant{
-	
+public class Enseignant {
+
 	private static final AtomicInteger count = new AtomicInteger();
 	private final int id;
 	private String nom;
 	private String prenom;
+	private String email;
 	private Specialite specialite;
 
-	public Enseignant(String nom, String prenom, Specialite specialite) {
+	public Enseignant(String nom, String prenom, String email, Specialite specialite) {
 		super();
 		this.id = count.incrementAndGet();
 		this.nom = nom;
 		this.prenom = prenom;
+		this.email = email;
 		this.specialite = specialite;
 	}
-	
+
 	public int getId() {
 		return id;
 	}
-	
+
 	public String getNom() {
 		return nom;
 	}
@@ -29,7 +31,7 @@ public class Enseignant{
 	public void setNom(String nom) {
 		this.nom = nom;
 	}
-	
+
 	public String getPrenom() {
 		return prenom;
 	}
@@ -37,15 +39,22 @@ public class Enseignant{
 	public void setPrenom(String prenom) {
 		this.prenom = prenom;
 	}
-	
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
 	public Specialite getSpecialite() {
 		return this.specialite;
 	}
-	
+
 	@Override
 	public String toString() {
 		return "Enseignant [id=" + id + ", nom=" + nom + ", prenom=" + prenom + ", specialite=" + specialite + "]";
 	}
-	
-}
 
+}
--- 18-03-2021/Exo2/Main.java
+++ 18-03-2021/Exo2/Main.java
@@ -9,7 +9,7 @@ public class Main {
 	public static void main(String[] args) {
 
 		ArrayList<Specialite> specialite = new ArrayList<Specialite>();
-		ArrayList<Enseignant> enseignants = new ArrayList<Enseignant>();
+		ArrayList<Enseignant> enseignant = new ArrayList<Enseignant>();
 
 		specialite.add(new Specialite("JAVA/JEE"));
 		specialite.add(new Specialite("HTML/CSS"));
@@ -17,34 +17,54 @@ public class Main {
 		specialite.add(new Specialite("JS"));
 		specialite.add(new Specialite("PHP"));
 
-		enseignants.add(new Enseignant("Toutain", "Damien", specialite.get(0)));
-		enseignants.add(new Enseignant("Micheaux", "Francis", specialite.get(0)));
-		enseignants.add(new Enseignant("Souchon", "Bernard", specialite.get(4)));
-		enseignants.add(new Enseignant("Astier", "Carla", specialite.get(3)));
+		enseignant.add(new Enseignant("Toutain", "Damien", "damien@gmail.com", specialite.get(0)));
+		enseignant.add(new Enseignant("Micheaux", "Francis", "francis@gmail.com", specialite.get(0)));
+		enseignant.add(new Enseignant("Souchon", "Bernard", "bernard@gmail.com", specialite.get(4)));
+		enseignant.add(new Enseignant("Astier", "Carla", "carla@gmail.com", specialite.get(3)));
 
-		HashMap<String, String> map = new HashMap<>();
-
+		/* Simple output */
 		boolean exists;
 		for (Specialite i : specialite) {
+			System.out.print(i.getNom() + ": ");
 			exists = false;
-			map.put(i.getNom(), "");
-			for (Enseignant j : enseignants) {
-				if (i.getNom().equals(j.getSpecialite().getNom())) {
-					String existing = map.get(i.getNom()) + " ";
-					String extraContent = "| " + j.getNom() + " " + j.getPrenom();
-					map.put(i.getNom(), existing == null ? extraContent : existing + extraContent);
+			for (Enseignant j : enseignant) {
+				if (i.getId() == (j.getSpecialite().getId())) {
+					System.out.print(" " + j);
 					exists = true;
 				}
 			}
 			if (!(exists)) {
+				System.out.print(" Aucun enseignant pour la spécialité ");
+			}
+			System.out.println();
+		}
+
+		System.out.println();
+
+		/* Map teachers to specialities */
+		HashMap<String, String> map = new HashMap<>();
+
+		boolean exist;
+		for (Specialite i : specialite) {
+			exist = false;
+			map.put(i.getNom(), "");
+			for (Enseignant j : enseignant) {
+				if (i.getNom().equals(j.getSpecialite().getNom())) {
+					String existing = map.get(i.getNom()) + " ";
+					String extraContent = j.toString();
+					map.put(i.getNom(), existing == null ? extraContent : existing + extraContent);
+					exist = true;
+				}
+			}
+			if (!(exist)) {
 				String existing = map.get(i.getNom()) + " ";
-				String extraContent = "| No teachers assigned";
+				String extraContent = " Aucun enseignant pour la spécialité";
 				map.put(i.getNom(), existing == null ? extraContent : existing + extraContent);
 			}
 		}
 
 		for (Entry<String, String> entry : map.entrySet()) {
-			System.out.println(entry.getKey() + "/" + entry.getValue());
+			System.out.println(entry.getKey() + ":" + entry.getValue());
 		}
 
 	}
