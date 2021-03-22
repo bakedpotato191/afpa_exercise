9a5e2d5851d12c41462adb1b57a8119072dd211a
bakedpotato191 <vladislavp@pm.me>
2021-03-22 12:06:20 +0100

    corrections

--- 22-03-2021/exercice_du_matin/Main.java
+++ 22-03-2021/exercice_du_matin/Main.java
@@ -5,10 +5,9 @@ public class Main {
 	public static void main(String[] args) {
 
 		Voiture v1 = new Voiture(4, "essence", 180, 60, 15);
-		Pompe p1 = new Pompe("essence", 650, 15);
+		Pompe p1 = new Pompe("essence", 650, 85);
 
 		Vehicule.fairePlein(p1, v1);
-
 	}
 
 }
--- 22-03-2021/exercice_du_matin/Vehicule.java
+++ 22-03-2021/exercice_du_matin/Vehicule.java
@@ -8,33 +8,40 @@ public class Vehicule {
 			if (obj1.getContenu() > (obj2.getContenanceReservoir() - obj2.getContenuReservoir())) {
 				toFill = obj2.getContenanceReservoir() - obj2.getContenuReservoir();
 
-				System.out.println("Total gas in pump: " + obj1.getContenu());
-				System.out.println("Tank capacity: " + obj2.getContenanceReservoir());
-				System.out.println("Current gas level " + obj2.getContenuReservoir());
-				System.out.println("Gas to be filled: " + toFill);
-				System.out.println("<---Filling the tank---->");
+				affiche_info_avant_remplissage(toFill, obj1.getContenu(), obj2.getContenanceReservoir(),
+						obj2.getContenuReservoir());
 
 				obj2.setContenuReservoir(obj2.getContenanceReservoir());
 				obj1.setContenu(obj1.getContenu() - toFill);
 
-				System.out.println("Remaining in pump: " + obj1.getContenu());
-				System.out.println("Currect gas level: " + obj2.getContenuReservoir());
-
+				affiche_info_apres_replissage(toFill, obj1.getContenu(), obj2.getContenuReservoir());
 			} else {
 				toFill = obj1.getContenu();
-				System.out.println("Total gas in pump: " + obj1.getContenu());
-				System.out.println("Tank capacity: " + obj2.getContenanceReservoir());
-				System.out.println("Current gas level " + obj2.getContenuReservoir());
-				System.out.println("Gas to fill: " + obj1.getContenu());
+
+				affiche_info_avant_remplissage(toFill, obj1.getContenu(), obj2.getContenanceReservoir(),
+						obj2.getContenuReservoir());
 
 				obj2.setContenuReservoir(obj2.getContenuReservoir() + toFill);
 				obj1.setContenu(0);
 
-				System.out.println("Remaining in pump: " + obj1.getContenu());
-				System.out.println("Gas in tank after the fill: " + obj2.getContenuReservoir());
+				affiche_info_apres_replissage(toFill, obj1.getContenu(), obj2.getContenuReservoir());
 			}
 		} else {
 			System.out.println("No pomp with the same carburant");
 		}
 	}
+
+	public static void affiche_info_avant_remplissage(int tf, int c1, int c2, int c3) {
+		System.out.println("Niveau actuel d'essence dans la pompe: " + c1);
+		System.out.println("Capacité du réservoir d'essence dans la voiture: " + c2);
+		System.out.println("Niveau d'essence actuel dans la voiture: " + c3);
+		System.out.println("Essence à remplir : " + tf);
+		System.out.println("<---Remplissage du Réservoir ---->");
+	}
+
+	public static void affiche_info_apres_replissage(int tf, int c1, int c2) {
+		System.out.println("Niveau d'essence dans la pompe après le remplissage: " + c1);
+		System.out.println("Niveau d'essence dans la voiture après le remplissage: " + c2);
+	}
+
 }
